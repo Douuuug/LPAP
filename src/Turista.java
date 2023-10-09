f5aae94e365a2356c78cea1e9016b32339a7b849
Douuuug <douglasmedeiros026@gmail.com>
2023-10-09 07:16:43 -0300

    Initial commit

---
+++ src/Turista.java
@@ -0,0 +1,16 @@
+public class Turista {
+    // Atributos
+     private String nome;
+     private String cpf;
+     // Encapsulamentos - Getters e Setters
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+
+}
